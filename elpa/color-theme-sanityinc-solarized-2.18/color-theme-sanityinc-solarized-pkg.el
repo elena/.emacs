d9b908445abeeef71471e818dff347eb95f0ea09
elena <ele.wil@gmail.com>
2013-02-13 16:54:25 +0800

    init

---
+++ elpa/color-theme-sanityinc-solarized-2.18/color-theme-sanityinc-solarized-pkg.el
@@ -0,0 +1,4 @@
+(define-package
+  "color-theme-sanityinc-solarized"
+  "2.18"
+  "A version of Ethan Schoonover's Solarized themes")
---
+++ elpa/color-theme-sanityinc-tomorrow-1.5/color-theme-sanityinc-tomorrow-pkg.el
@@ -0,0 +1,4 @@
+(define-package
+  "color-theme-sanityinc-tomorrow"
+  "1.5"
+  "A version of Chris Kempson's various Tomorrow themes")
