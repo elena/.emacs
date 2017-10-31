a535e18e44b8ffbb7310fdbaaad8fa59c8f814f4
Elena Williams <git@elena.net.au>
2017-11-01 10:41:56 +1100

    Update .gitignore and other trivial custom

--- init.dead.el
+++ init.dead.el
@@ -1,3 +1,48 @@
+(package-initialize)
+
+(let ((default-directory "~/.emacs.d/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/") t)
+
+;; ;; (require 'package)
+;; ;; (add-to-list 'package-archives
+;; ;;              '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+;; ;; (add-to-list 'package-archives
+;; ;;              '("melpa" . "http://melpa.org/packages/"))
+;; ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+;; ;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
+;; ;;                          ("melpa" . "http://melpa.org/packages/")))
+
+(defvar my-packages
+  '(better-defaults
+    auto-complete
+    auto-complete-config
+    ;; color-theme
+    elpy
+    ;; exec-path-from-shell
+    ;; flycheck
+    ;; flycheck-pyflakes
+    ;; js3-mode
+    ;; magit
+    ;; markdown-mode
+    ;; markdown-preview-mode
+    py-autopep8
+    ;; rainbow-delimiters
+    recentf
+    ;; smart-mode-line
+    ;; web-mode
+    ;; yaml-mode
+    ;; yasnippet
+    ))
+
+;; ;; (when (not package-archive-contents)
+;; ;;     (package-refresh-contents))
+;; ;; (package-initialize)
+
+
+
+
 
 
 ;; (require 'golden-ratio)
@@ -36,3 +81,9 @@
 ;; (require 'epy-nose)       ;; For nose integratio
 ;; (epy-setup-ipython)
 ;; (epy-setup-checker "pyflakes %f")
+
+
+;; * rainbow-mode *
+;; ----------------
+(add-hook 'less-css-mode-hook '(lambda () (rainbow-mode 1)))
+(add-hook 'css-mode-hook '(lambda () (rainbow-mode 1)))
